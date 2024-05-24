9af604ec65b14ad266594d7d093cab8fdeae4c54
itc-s23012 <s23012@std.it-college.ac.jp>
2024-05-24 12:28:12 +0900

    d48Q5kadai

---
+++ d48Q5kadai.java
@@ -0,0 +1,24 @@
+// s23012
+// どんな課題プログラム化を記入する int配列を降順にソートしている
+public class d48Q5kadai {
+   public static void main(String[] args) {
+	  int[] arr = {11, 22, 33, 44, 55};
+	  int n = arr.length;
+
+	  for(int i=0; i<n-1; i++){
+		  int maxIndex = i;
+		  for(int j=i+1; j<n; j++){
+			  if(arr[j] > arr[maxIndex]){
+			      maxIndex = j;
+			  }
+		  int tmp = arr[maxIndex];
+		  arr[maxIndex] = arr[i];
+		  arr[i] = tmp;
+		  }
+	  }
+	  for(int num : arr) {
+	       System.out.println(num);
+	  }
+	}
+}
+	  
