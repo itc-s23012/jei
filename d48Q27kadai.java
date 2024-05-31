518123927548cd2fdaeabe7686dc8416c7b2aec3
itc-s23012 <s23012@std.it-college.ac.jp>
2024-05-31 09:39:49 +0900

    5/24kadai

---
+++ d48Q27kadai.java
@@ -0,0 +1,9 @@
+public class d48Q27kadai {
+   public static void main(String[] args) {
+	  int i = 0;
+	  while (i <= 6) {
+		 System.out.print(i);
+		 i++;
+	  }
+   }
+}
--- d48Q2kadai.java
+++ d48Q2kadai.java
@@ -28,8 +28,10 @@ public class d48Q2kadai {
     public static void main(String[] args) {
         Animal a = new Dog(); //インスタンス化
 	    Animal b = new Cat();
+		Animal c = new Caw();
         a.makeSound(); //どのような処理になるか
         a.test(); //どのような処理になるか
 	    b.makeSound();
+		c.makeSound();
     }
 }
